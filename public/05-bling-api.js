e625407ec2c97f272f3aef75f372231bcab13503
dimarquesmachado-sys <dimarquesmachado@gmail.com>
2026-09-09 14:25:56 +0100

    fix(historico): invalidar o indice tambem nas mutacoes in-place (push/unshift)

--- public/05-bling-api.js
+++ public/05-bling-api.js
@@ -406,6 +406,7 @@ function adicionarPedidoBling(blingId){
       date:today,pulledAt:nowTS(),
       status:'pendente',colT:null,obs:'',blingId:o.id
     });
+  if(typeof invalidarIndicePkgs==='function') invalidarIndicePkgs();
   }
   sv('expv5_pkgs',packages);
   // O Bling devolveu ESTE pedido agora: autoriza o servidor a aceitá-lo mesmo se
@@ -509,6 +510,7 @@ function pullFromBlingMkt(mkt){
             dataPedido:o.data||'',date:today,pulledAt:nowTS(),
             status:'pendente',colT:null,obs:'',blingId:o.id
           });
+  if(typeof invalidarIndicePkgs==='function') invalidarIndicePkgs();
           novos++;
         } else {
           // Atualiza campos que podem ter chegado
